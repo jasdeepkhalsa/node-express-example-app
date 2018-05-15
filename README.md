11d9a0acfefc5778184ff1333b4363f8591013dd
Glitch (node-express-example-app) <none>
2018-05-15 10:14:25 +0000

    🌲😺 Updated with Glitch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Node.js & Express Example App
 
 Provides a best practice example of how to use Node.js and Express together using modern JavaScript ES6 features including `async`/`await`
+
+[![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/import/github/jhbsk/node-express-example-app)
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 // Other routes
-router.use('/api', require('./api'))
+router.use('/sync-api', require('./sync-api'))
+router.use('/async-fs', require('./async-fs'))
+router.use('/async-github', require('./async-github'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 router.get('/', (req, res) => res.render('home', { name: req.query.name }))
